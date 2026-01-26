bb27effd090a59660667cc23e3c411ea0ef0f58c
sunbreak1211 <sunbreak1211@proton.me>
2026-01-26 17:02:25 -0300

    Add comment

--- src/BeamState.sol
+++ src/BeamState.sol
@@ -216,6 +216,9 @@ contract BeamState {
         emit DelCBeam(cBeam);
     }
 
+    // Note: Once cBEAMS are whitelisted they can be assigned to controllers/rate-limits (potentially without delay),
+    // so theoretically they can be positioned to interfere with each other. This is known and assumed to be monitored
+
     function setCBeamForRateLimits(address rateLimits_, address cBeam) external roleAuth {
         require(rateLimits[rateLimits_] == 1, "BeamState/not-existing-rateLimits");
         require(cBeams[cBeam] == 1, "BeamState/not-existing-cBeam");
