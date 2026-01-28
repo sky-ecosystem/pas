95a5ab6e3c8725e140fedd6fef8afd1b647c5061
oldchili <130549691+oldchili@users.noreply.github.com>
2026-01-28 21:44:58 +0700

    Change l2 spell pragma to ^0.8.24

--- deploy/L2PASSpell.sol
+++ deploy/L2PASSpell.sol
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.24;
 
 import { PASInit } from "./PASInit.sol";
 import { PASInstance } from "./PASInstance.sol";
